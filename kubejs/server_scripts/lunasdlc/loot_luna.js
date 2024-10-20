b112ef8b706d8c4d87e124185da463278eefeb77
李诗琪 <120086484+LunaGlaze@users.noreply.github.com>
2024-10-20 22:59:26 +0800

    为超凡胶囊添加BOSS掉落
    通过LootJS修改，使得玩家可以在体内具有至少一件超凡器官时，通过击败12难神意BOSS获取超凡胶囊掉落（你包逃课不难的，真的）。
    退坑了工程都删了，只有github网页版，所以不会更新安装包，至少暂时在夜莺出下版本前或者有冤大头请我回来之前不会，有需要自行下载仓库文件替换。

--- kubejs/server_scripts/lunasdlc/loot_luna.js
+++ kubejs/server_scripts/lunasdlc/loot_luna.js
@@ -1,5 +1,32 @@
 // priority: 100
 LootJS.modifiers(event => {
+    
+    function addBossLootLuna(entity) {
+        return event.addEntityLootModifier(entity)
+            .apply(ctx => {
+                let entity = ctx.entity
+                let diffLevelNum = 1
+                if (entity.persistentData.contains('diffLevel')) {
+                    diffLevelNum = entity.persistentData.getInt('diffLevel')
+                }
+                if (diffLevelNum >= 12 &&
+                (entity.hasEffect('kubejs:glare_of_god') ||
+                    entity.hasEffect('kubejs:gaze_of_god') ||
+                    entity.hasEffect('kubejs:glimpse_of_god'))) {
+                    if (ctx.player) {
+                        let typeMap = getPlayerChestCavityTypeMap(ctx.player);
+                        if (typeMap.has('kubejs:archotech')){
+                            ctx.player.give(Item.of('luna_flesh_reforged:archotech_capsule'))
+                        }
+                    }
+                }
+            })
+    }
+    
+    bossTypeList.forEach(entityId => {
+        addBossLootLuna(entityId)
+    })
+    
     event.addEntityLootModifier('minecraft:zombie')
         .addLoot(LootEntry.of('luna_flesh_reforged:zombie_brain').when((c) => c.randomChance(0.1)));
 
